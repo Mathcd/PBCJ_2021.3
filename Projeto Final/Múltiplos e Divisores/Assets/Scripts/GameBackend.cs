22aa390a30f500eea0e499f4fc2b220424d2dc05
Dimitri Leandro <dimitri.leandro@gmail.com>
2021-12-09 20:29:57 -0300

    finalizando o backend do jogo e simulação máquina vs máquina

--- Projeto Final/Múltiplos e Divisores/Assets/Scripts/GameBackend.cs
+++ Projeto Final/Múltiplos e Divisores/Assets/Scripts/GameBackend.cs
@@ -1,7 +1,20 @@
+using Random = System.Random;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace GameBackend
 {
+    public static class Globals
+    {
+        public static int QTD_CARTAS_AO_CRIAR_BARALHO = 5;
+        public static int QTD_MAX_CARTAS_MAO = 15;
+        public static char[] TIPOS_DISPONIVEIS = new char[] {'M', 'D', 'A', 'A'};
+        public static int[] MULTIPLOS_DISPONIVEIS = new int[] {1, 2, 3, 4, 5, 6, 8, 9, 10};
+        public static int[] DIVISORES_DISPONIVEIS = new int[] {6, 8, 9, 10, 12, 15, 16, 18, 20, 24};
+        public static int[] AMBOS_DISPONIVEIS = new int[] {6, 8, 10};
+        public static Random RANDOM = new Random();
+    }
+
     public class Carta
     {
         private int valor;
@@ -11,7 +24,6 @@ namespace GameBackend
         {
             this.setValor(valor);
             this.setTipo(tipo);
-            Debug.Log("Construtor de Carta chamado: " + this.getValor() + this.getTipo());
         }
 
         public void setValor(int valor)
@@ -32,6 +44,168 @@ namespace GameBackend
         public char getTipo()
         {
             return this.tipo;
+        }
+
+        public string getNome()
+        {
+            string carta = this.getValor() + "" + this.getTipo() + ", ";
+            return carta;
+        }
+    }
+
+    public abstract class Baralho
+    {
+        private List<Carta> cartas;
+        private int qtdCartas;
+
+        protected void setCartas(int qtd)
+        {
+            char tipo = 'M';
+            int valor = 1;
+            this.setQtdCartas(qtd);
+            this.cartas = new List<Carta>();
+            for (int i=0; i<qtd; i++)
+            {
+                tipo = Globals.TIPOS_DISPONIVEIS[Globals.RANDOM.Next(Globals.TIPOS_DISPONIVEIS.Length)];
+                switch(tipo)
+                {
+                    case 'M':
+                        valor = Globals.MULTIPLOS_DISPONIVEIS[Globals.RANDOM.Next(Globals.MULTIPLOS_DISPONIVEIS.Length)];
+                        break;
+                    case 'D':
+                        valor = Globals.DIVISORES_DISPONIVEIS[Globals.RANDOM.Next(Globals.DIVISORES_DISPONIVEIS.Length)];
+                        break;
+                    case 'A':
+                        valor = Globals.AMBOS_DISPONIVEIS[Globals.RANDOM.Next(Globals.AMBOS_DISPONIVEIS.Length)];
+                        break;
+                }
+                this.cartas.Add(new Carta(valor, tipo));
+            }
+        }
+
+        protected Carta getCartaTopo()
+        {
+            Carta cartaTopo = this.getCartaAt(this.getQtdCartas()-1);
+            Debug.Log("Carta do topo: " + cartaTopo.getNome());
+            return cartaTopo;
+        }
+
+        public void adicionarCartaComprada(Carta carta)
+        {
+            Debug.Log("Adicionando carta " + carta.getNome() + " ao baralho");
+            this.cartas.Add(carta);
+            this.setQtdCartas(this.getQtdCartas() + 1);
+            if (this.getQtdCartas() > Globals.QTD_MAX_CARTAS_MAO)
+            {
+                Debug.Log("Quantidade máxima de cartas atingida. Uma aleatória será removida");
+                this.cartas.RemoveAt(Globals.RANDOM.Next(Globals.QTD_MAX_CARTAS_MAO));
+                this.setQtdCartas(Globals.QTD_MAX_CARTAS_MAO);
+            }
+        }
+
+        public void jogarCarta(int index)
+        {
+            this.cartas.RemoveAt(index);
+            this.setQtdCartas(this.getQtdCartas() - 1);
+        }
+
+        public void printarBaralho(string prefixo)
+        {
+            string baralho = prefixo + ": ";
+            for (int i=0; i<this.getQtdCartas(); i++)
+            {
+                baralho += this.cartas[i].getValor();
+                baralho += this.cartas[i].getTipo();
+                baralho += ", ";
+            }
+            Debug.Log(baralho);
+        }
+
+        protected void setQtdCartas(int qtdCartas)
+        {
+            this.qtdCartas = qtdCartas;
+        }
+
+        public int getQtdCartas()
+        {
+            return this.qtdCartas;
+        }
+
+        public Carta getCartaAt(int index)
+        {
+            return this.cartas[index];
+        }
+    }
+
+    public class PilhaCompra : Baralho
+    {
+        public PilhaCompra()
+        {
+            this.setCartas(Globals.QTD_CARTAS_AO_CRIAR_BARALHO);
+        }
+
+        public void verificarPilhaVazia()
+        {
+            if (this.getQtdCartas() == 0)
+            {
+                Debug.Log("Pilha vazia. Uma nova será gerada aleatóriamente");
+                this.setCartas(Globals.QTD_CARTAS_AO_CRIAR_BARALHO);
+            }
+        }
+
+        public Carta comprarCarta()
+        {
+            Carta topo = this.getCartaTopo();
+            this.setQtdCartas(this.getQtdCartas() - 1);
+            this.verificarPilhaVazia();
+            Debug.Log("Carta comprada: " + topo.getNome());
+            return topo;
+        }
+    }
+
+    public class Maquina : Baralho
+    {
+        public Maquina()
+        {
+            this.setCartas(Globals.QTD_CARTAS_AO_CRIAR_BARALHO);
+        }
+
+        public int procurarCartaCompativel(Carta anterior)
+        {
+            char tipoAnterior = anterior.getTipo();
+            int valorAnterior = anterior.getValor();
+            int valorAtual;
+
+            for (int i=0; i<this.getQtdCartas(); i++)
+            {
+                valorAtual = this.getCartaAt(i).getValor();
+
+                switch(anterior.getTipo())
+                {
+                    case 'M':
+                        if (valorAtual % valorAnterior == 0)
+                            return i;
+                        break;
+                    case 'D':
+                        if (valorAnterior % valorAtual == 0)
+                            return i;
+                        break;
+                    case 'A':
+                        if (valorAnterior % valorAtual == 0 | valorAtual % valorAnterior == 0)
+                            return i;
+                        break;
+                }              
+            }
+
+            return -1;
+        }
+    }
+
+    public class Jogador : Baralho
+    {
+        public Jogador()
+        {
+            this.setCartas(Globals.QTD_CARTAS_AO_CRIAR_BARALHO);
         }
     }
 }
--- Projeto Final/Múltiplos e Divisores/Assets/Scripts/Main.cs
+++ Projeto Final/Múltiplos e Divisores/Assets/Scripts/Main.cs
@@ -9,13 +9,120 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Construtor de Main");
-        Carta carta = new Carta(1, 'M');
+        int rodadas;
+        for(int i=0; i<1; i++)
+        {
+            rodadas = jogoCompleto(500);
+            Debug.Log("JOGO FINALIZADO EM: " + rodadas + " RODADAS");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int jogoCompleto(int qtdMaxRodadas)
     {
-        
+        // Variaveis 
+        int indexCarta, rodadas=0;
+        Carta novaCarta;
+
+        // Criando um baralho para a pilha de compra
+        PilhaCompra pilha = new PilhaCompra();
+        pilha.printarBaralho("pilha");
+
+        // Criando um baralho para a máquina 1
+        Maquina maquina1 = new Maquina();
+        maquina1.printarBaralho("máquina 1");
+
+        // Criando um baralho para a máquina 2
+        Maquina maquina2 = new Maquina();
+        maquina2.printarBaralho("máquina 2");
+
+        // Definindo a carta que vai comecar o jogo (fazer isso ser aleatorio depois)
+        Carta cartaAnterior = new Carta(2, 'A');
+
+        // Rodadas do jogo
+        do
+        {
+            rodadas++;
+
+            // Maquina 1 verifica se tem alguma carta compativel em seu baralho
+            // se nao tiver, entao compra uma carta da pilha de compra
+            // e verifica se consegue jogar com a carta comprada, senao passa a vez
+            Debug.Log("Máquina 1 iniciando sua jogada");
+            
+            // Procuro uma carta compativel
+            indexCarta = maquina1.procurarCartaCompativel(cartaAnterior);
+            
+            // Se encontrou uma carta...
+            if (indexCarta >= 0)
+            {
+                // Aqui achamos uma carta no baralho, vou jogar a carta encontrada
+                // e definir que agr a carta anterior eh a carta encontrada
+                Debug.Log("Máquina 1 encontrou carta no index " + indexCarta);
+                cartaAnterior = maquina1.getCartaAt(indexCarta);
+                maquina1.jogarCarta(indexCarta);
+            }
+
+            // Se nao encontrou, compra
+            else
+            {
+                Debug.Log("Máquina 1 não encontrou carta compatível e comprará uma carta da pilha.");
+                novaCarta = pilha.comprarCarta();
+                maquina1.adicionarCartaComprada(novaCarta);
+
+                // Depois de comprar, verifica se consegue jogar, senao passa a vez
+                indexCarta = maquina1.procurarCartaCompativel(cartaAnterior);
+                if (indexCarta >= 0)
+                {
+                    // Aqui achamos uma carta no baralho, vou jogar a carta encontrada
+                    // e definir que agr a carta anterior eh a carta encontrada
+                    Debug.Log("Máquina 1 encontrou carta no index " + indexCarta);
+                    cartaAnterior = maquina1.getCartaAt(indexCarta);
+                    maquina1.jogarCarta(indexCarta);
+                }
+            }
+
+            // Finaliza a jogada
+            Debug.Log("Máquina 1 finalizou sua jogada");
+            maquina1.printarBaralho("máquina 1");
+
+            // Mesma coisa pra máquina 2
+            Debug.Log("Máquina 2 iniciando sua jogada");
+            indexCarta = maquina2.procurarCartaCompativel(cartaAnterior);
+            if (indexCarta >= 0)
+            {
+                Debug.Log("Máquina 2 encontrou carta no index " + indexCarta);
+                cartaAnterior = maquina2.getCartaAt(indexCarta);
+                maquina2.jogarCarta(indexCarta);
+            }
+            else
+            {
+                Debug.Log("Máquina 2 não encontrou carta compatível e comprará uma carta da pilha.");
+                novaCarta = pilha.comprarCarta();
+                maquina2.adicionarCartaComprada(novaCarta);
+                indexCarta = maquina2.procurarCartaCompativel(cartaAnterior);
+                if (indexCarta >= 0)
+                {
+                    Debug.Log("Máquina 2 encontrou carta no index " + indexCarta);
+                    cartaAnterior = maquina2.getCartaAt(indexCarta);
+                    maquina2.jogarCarta(indexCarta);
+                }
+            }
+            Debug.Log("Máquina 2 finalizou sua jogada");
+            maquina2.printarBaralho("máquina 2");
+        } while (maquina1.getQtdCartas() > 0 & maquina2.getQtdCartas() > 0 & rodadas < qtdMaxRodadas);
+
+        if (maquina1.getQtdCartas() == 0 )
+        {
+            Debug.Log("MÁQUINA 1 VENCEU O JOGO EM " + rodadas + " RODADAS!");
+        }
+        else if (maquina2.getQtdCartas() == 0 )
+        {
+            Debug.Log("MÁQUINA 2 VENCEU O JOGO EM " + rodadas + " RODADAS!");
+        }
+        else
+        {
+            Debug.Log("JOGO PARADO POIS ATINGIMOS " + rodadas + " RODADAS");
+        }
+
+        return rodadas;
     }
 }
