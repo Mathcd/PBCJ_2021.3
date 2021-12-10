2b6b0d4c0d71ea029b65a141df99b1bf88e1ad9a
Matheus Costa <matheuscosta2004@hotmail.com>
2021-12-09 21:05:04 -0300

    Last saved state

---
+++ Lab 05_09 - RPG/RPG/Assets/Scripts/Monobehaviours/Caractere.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Caractere : MonoBehaviour // abstract indica que aclasse não pode ser instaciada, e sim herdada
+{
+    public int PontosDano;
+    public int MaxPontosDano;
+
+}
